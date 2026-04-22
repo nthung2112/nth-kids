1900adcf76a3e0636c62501e2e39b05777357a4e
Hung Nguyen <nthung2112@gmail.com>
2026-04-22 21:46:47 +0700

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
-
-import { staticRoutesPrerender } from "./scripts/vite-plugin-static-routes.mjs";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const baseUrl = mode === "development" ? "/" : env.VITE_BASE_URL || "/";
-
+export default defineConfig(() => {
   return {
-    base: baseUrl,
     plugins: [
       tailwindcss(),
       tanstackRouter({
@@ -20,9 +14,6 @@ export default defineConfig(({ mode }) => {
         autoCodeSplitting: true,
       }),
       react(),
-      staticRoutesPrerender({
-        routeTreePath: path.resolve(__dirname, "./src/routeTree.gen.ts"),
-      }),
     ],
     resolve: {
       alias: {
